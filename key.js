06373c59d43c071b89014f3bb1b4a0e6a242295c
georg <PuuRaidur@users.noreply.github.com>
2025-11-16 10:32:06 +0200

    Key-value functions

--- key.js
+++ key.js
@@ -7,5 +7,17 @@ function getValueFromKey(obj, key) {
 }
 
 function addKeyValuePairs(obj, key_value) {
-    return obj.assign(obj, key_value)
+    Object.assign(obj, key_value);
+    return obj;
 }
+
+const location = {
+    city: 'Tallinn',
+    country: 'Estonia',
+}
+
+
+console.log(getValueFromKey(location, 'city'))
+console.log(getValueFromKey(location, 'continent'))
+console.log(addKeyValuePairs(location, { airports: ['TLL']}))
+console.log(addKeyValuePairs(location, { highestElevation: 64 }))
