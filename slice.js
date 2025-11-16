6eeee7512fea6dcbf978a0df787705121951fa12
georg <PuuRaidur@users.noreply.github.com>
2025-11-16 12:29:28 +0200

    Content extracting function

--- slice.js
+++ slice.js
@@ -1,14 +1,3 @@
 function extractContent(content, start, end) {
-    return content => content.slice(Array.indexOf(start), Array.lastIndexOf(end))
+    return content.slice(content.indexOf(start), content.lastIndexOf(end) + 1)
 }
-
-const blogPosts = ['post1', 'post2', 'post3', 'post4', 'post5'];
-const blogPost = 'The big brown fox';
-
-extractContent(blogPosts, 'post2', 'post4');
-extractContent(blogPosts, '', '');
-extractContent(blogPost, '', '');
-extractContent(blogPost, 'b', 'b');
-extractContent(blogPost, 'big', 'fox');
-extractContent(blogPost, '', 'brown');
-extractContent(blogPost, 'jump', '');
