67e982c81c6f8593bec4a0ffae2be1ee8e5e4062
georg <PuuRaidur@users.noreply.github.com>
2025-11-16 09:50:32 +0200

    Key-value functions

---
+++ key.js
@@ -0,0 +1,11 @@
+function getValueFromKey(obj, key) {
+    if(obj.key === null) {
+        return undefined;
+    } else {
+        return obj.key
+    }
+}
+
+function addKeyValuePairs(obj1, obj2) {
+    return obj1 + obj2
+}
