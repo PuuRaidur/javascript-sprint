743a39326469781702f78b9088509a4f30672743
georg <PuuRaidur@users.noreply.github.com>
2025-11-16 12:13:32 +0200

    Reference concept

---
+++ itself.js
@@ -0,0 +1,14 @@
+const sender = {
+    name: "Tom",
+    address: "123 Main St",
+}
+
+const deliveryPackage = {
+    contents: "porridge",
+    sender: sender,
+}
+
+const trackingInfo = {
+    status: "in transit",
+    sender: sender,
+}
