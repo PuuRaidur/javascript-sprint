d2f23ef9fbe48ff2412915af24806057ca0fb3b5
georg <PuuRaidur@users.noreply.github.com>
2025-11-25 15:07:39 +0200

    get elements

--- get-el.js
+++ get-el.js
@@ -13,9 +13,9 @@ function getElementById(id) {
 function getElementsByAttribute(attrName, attrValue) {
     if (attrValue === undefined) {
         // Match elements that have the attribute (regardless of value)
-        return document.querySelectorAll(`[${CSS.escape(attrName)}]`);
+        return document.querySelectorAll(`[${attrName}]`);
     } else {
         // Match elements with the specific attribute=value
-        return document.querySelectorAll(`[${CSS.escape(attrName)}="${CSS.escape(attrValue)}"]`);
+        return document.querySelectorAll(`[${attrName}="${attrValue}"]`);
     }
 }
